e61093e7f44671c0564775e4b5f1c64e9b2f118d
Rutvi Dabhi <rutvidabhi93@gmail.com>
2026-07-20 12:13:26 +0530

    Initial commit: Spring Boot hello-user-app with /hello endpoint

---
+++ src/main/java/com/example/hellouserapp/HelloUserAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellouserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUserAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellouserapp/HelloUserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellouserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
